9e7d74f1712cdf3f56ecbbbad0e20623ffe5890e
Vinícius Alexandre <mano19vini@gmail.com>
2020-04-07 15:50:03 -0300

    add info front and back

--- simple-gym-2/backend/src/main/java/br/com/simplegym/simplegym/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.simplegym.simplegym.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.simplegym.simplegym.entity.User;
- 
-public interface UserRepository extends JpaRepository <User, Integer> {
-
-}
